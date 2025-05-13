7b37d11f95bc922dd63ae03a7e62768191864854
nirob019207 <nirobhasanbd01955@gmial.com>
2025-05-14 00:29:21 +0600

    navbar and hereo and footer section is complete

--- src/shared/Footer.tsx
+++ src/shared/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="relative w-full bg-blue-900/30 backdrop-blur-sm border-t border-white/10">
+    <footer className="relative w-full bg-blue-800 backdrop-blur-sm border-t border-white/10">
       <div className="container mx-auto px-6 py-8">
         <div className="pt-8 border-t border-white/10 flex flex-col md:flex-row justify-between items-center">
           <p className="text-blue-100/80 text-sm text-center">
